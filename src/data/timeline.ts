440e5fafc63f363579ccdb70ebef1a846617f161
void2610 <izumisan2610@gmail.com>
2025-06-01 21:39:39 +0900

    Add timeline contents

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -6,43 +6,38 @@ export interface TimelineItem {
 
 export const timelineData: TimelineItem[] = [
   {
-    date: "2023年12月",
-    title: "ポートフォリオサイト公開",
-    description: "Next.js 15とTypeScriptで構築したポートフォリオサイトを公開",
+    date: "2024年8月",
+    title: "MergeRogue(仮称) 開発開始",
+    description: "Steamでの有料販売を目指して開発中",
   },
   {
-    date: "2023年8月",
-    title: "新作ゲームをSteamでリリース",
-    description: "半年間の開発期間を経て、アクションゲームをSteamで公開",
+    date: "2024年1月",
+    title: "ゲーム開発を本格化",
+    description: "unity1weekをはじめとしたゲームジャムに参加",
   },
   {
-    date: "2023年4月",
-    title: "フリーランスとして独立",
-    description: "ゲーム開発とWeb開発の両軸で活動開始",
+    date: "2023年9月",
+    title: "燈株式会社にてインターン開始",
+    description: "TypeScriptやPythonを用いたAIプロダクトの開発に従事",
+  },
+  {
+    date: "2023年3月",
+    title: "起業家甲子園に出場",
+    description: "シリコンバレーでの研修プログラムにも参加",
   },
   {
     date: "2022年10月",
-    title: "Web開発の学習開始",
-    description: "ReactとTypeScriptを中心にモダンなWeb開発技術を習得",
+    title: "高専プロコンにて特別賞受賞",
+    description: "第33回全国高専プロコン 課題部門にて、特別賞, NICT賞を受賞",
   },
   {
-    date: "2021年11月",
-    title: "unityroomゲームコンテスト 入賞",
-    description: "「ゲームタイトル」で特別賞を受賞、1万プレイ達成",
-  },
-  {
-    date: "2021年4月",
-    title: "初のゲームリリース",
-    description: "unityroomにて初めての作品を公開",
-  },
-  {
-    date: "2020年6月",
-    title: "Unity学習開始",
-    description: "ゲーム開発エンジンUnityでの開発を本格的に開始",
+    date: "2020年4月",
+    title: "コンテスト参加",
+    description: "プログラミング研究会にて、ゲームコンテストやハッカソンに参加",
   },
   {
     date: "2019年4月",
-    title: "プログラミング学習開始",
-    description: "大学でC言語とPythonの基礎を学ぶ",
+    title: "福井高専入学",
+    description: "電子情報工学科に進学、プログラミングの基礎を学ぶ",
   },
 ];
