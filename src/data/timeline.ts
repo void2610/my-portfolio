e7726390275902b309e30791ad0e336c1c9bac60
void2610 <izumisan2610@gmail.com>
2025-06-02 19:07:25 +0900

    Update timeline.ts

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -13,7 +13,7 @@ export const timelineData: TimelineItem[] = [
   {
     date: "2024年1月",
     title: "ゲーム開発を本格化",
-    description: "unity1weekをはじめとしたゲームジャムに参加",
+    description: "unity1weekをはじめとしたゲームジャムへ参加。「評価の高かった作品」に選ばれる",
   },
   {
     date: "2023年9月",
@@ -26,18 +26,23 @@ export const timelineData: TimelineItem[] = [
     description: "シリコンバレーでの研修プログラムにも参加",
   },
   {
+    date: "2022年12月",
+    title: "福井高専ビジネスアイデアコンテストにてグランプリ獲得",
+    description: "台湾での研修プログラムに参加",
+  },
+  {
     date: "2022年10月",
     title: "高専プロコンにて特別賞受賞",
     description: "第33回全国高専プロコン 課題部門にて、特別賞, NICT賞を受賞",
   },
   {
     date: "2020年4月",
-    title: "コンテスト参加",
-    description: "プログラミング研究会にて、ゲームコンテストやハッカソンに参加",
+    title: "多数のコンテストに参加",
+    description: "プログラミング研究会にて、ゲームコンテストやハッカソンに参加。企業賞などを受賞",
   },
   {
     date: "2019年4月",
-    title: "福井高専入学",
+    title: "福井高専に入学",
     description: "電子情報工学科に進学、プログラミングの基礎を学ぶ",
   },
 ];
